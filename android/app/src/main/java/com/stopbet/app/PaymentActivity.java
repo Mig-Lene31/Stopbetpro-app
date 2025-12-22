3f57cd0360f5e311a5642cb0cb10a3ad981f4456
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-22 13:39:28 -0300

    Teste mínimo pós-reboot: garantir abertura do app

--- android/app/src/main/java/com/stopbet/app/PaymentActivity.java
+++ android/app/src/main/java/com/stopbet/app/PaymentActivity.java
@@ -1,7 +1,6 @@
 package com.stopbet.app;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,9 +10,9 @@ public class PaymentActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Teste mínimo para ver se o app abre
         TextView tv = new TextView(this);
-        tv.setText("StopBet Pro - teste de abertura OK");
+        tv.setText("STOPBET PRO\nAPP ABRIU COM SUCESSO");
+        tv.setTextSize(20);
         setContentView(tv);
     }
 }
