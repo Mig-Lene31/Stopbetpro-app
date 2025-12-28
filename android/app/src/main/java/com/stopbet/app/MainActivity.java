56dfd9416b266015a03495e88ae5603f51041606
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-28 13:11:16 -0300

    FIX: código limpo sem lixo de terminal

--- android/app/src/main/java/com/stopbet/app/MainActivity.java
+++ android/app/src/main/java/com/stopbet/app/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends Activity {
 
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setPadding(40,40,40,40);
+        layout.setPadding(40, 40, 40, 40);
 
         TextView title = new TextView(this);
         title.setText("StopBet Pro");
