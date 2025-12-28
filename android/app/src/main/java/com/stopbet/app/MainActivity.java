c7bdfed2be3f990d19879959282054c7d4d06b48
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-28 13:40:46 -0300

    FIX: MainActivity corrigida e válida

--- android/app/src/main/java/com/stopbet/app/MainActivity.java
+++ android/app/src/main/java/com/stopbet/app/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
