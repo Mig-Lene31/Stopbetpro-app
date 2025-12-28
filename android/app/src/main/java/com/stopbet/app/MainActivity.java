f366d130a4ee09306d6c6a85b4823967345f53f9
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-28 13:19:18 -0300

    FIX: corrige imports da MainActivity

--- android/app/src/main/java/com/stopbet/app/MainActivity.java
+++ android/app/src/main/java/com/stopbet/app/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
