1ba805bebb315512ed9db9f6bd4e6956c061b904
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-29 18:46:16 -0300

    CORE: Accessibility Service para bloqueio automático de sites de apostas

---
+++ android/app/src/main/java/com/stopbet/app/BetSites.java
@@ -0,0 +1,22 @@
+package com.stopbet.app;
+
+public class BetSites {
+
+    public static final String[] LIST = {
+        "bet",
+        "blaze",
+        "pixbet",
+        "betano",
+        "sportingbet",
+        "bet365",
+        "betfair",
+        "stake",
+        "1xbet",
+        "parimatch",
+        "betway",
+        "novibet",
+        "vbet",
+        "kto",
+        "cassino"
+    };
+}
