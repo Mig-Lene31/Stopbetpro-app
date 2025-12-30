3e7530ab09ba3ce13ed7c1631dcca5a5c773bdad
Mig-Lene31 <Mislene.souss789@gmail.com>
2025-12-30 09:41:45 -0300

    fix: corrige package inválido em GateActivity

--- android/app/src/main/java/com/stopbet/app/GateActivity.java
+++ android/app/src/main/java/com/stopbet/app/GateActivity.java
@@ -1,5 +1,4 @@
-ackage com.stopbet.app;
-
+package com.stopbet.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,19 +9,8 @@ public class GateActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Inicializações mínimas defensivas
-        AppPrefs.ensureDefaults(this);
-        MotorState.ensureDefaults(this);
-
-        // Se estiver bloqueado, vai direto pra tela azul
-        if (EngineState.isBlocked(this)) {
-            startActivity(new Intent(this, BlockedActivity.class));
-            finish();
-            return;
-        }
-
-        // Fluxo normal
-        startActivity(new Intent(this, PaymentActivity.class));
+        // Fluxo inicial limpo e estável
+        startActivity(new Intent(this, MainActivity.class));
         finish();
     }
 }
