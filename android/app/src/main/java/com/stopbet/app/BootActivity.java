49317914b88786d7cf1711a9c5b293c384cb7700
Mig-Lene31 <Mislene.souss789@gmail.com>
2026-01-07 11:14:51 -0300

    CORE: recuperação automática ao iniciar o app

--- android/app/src/main/java/com/stopbet/app/BootActivity.java
+++ android/app/src/main/java/com/stopbet/app/BootActivity.java
@@ -2,55 +2,22 @@ package com.stopbet.app;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.net.VpnService;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class BootActivity extends Activity {
-
-    private static final int VPN_REQUEST_CODE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        startService(new Intent(this, EngineRecoveryService.class));
+
         if (EngineState.isBlocked(this)) {
-            ensureVpnPermission();
             startActivity(new Intent(this, BlockedActivity.class));
-            finish();
-            return;
+        } else {
+            startActivity(new Intent(this, GateActivity.class));
         }
 
-        TextView tv = new TextView(this);
-        tv.setText("STOPBET PRO\nVERSÃO 2.0.3\nBOOT OK");
-        tv.setTextSize(22);
-        tv.setPadding(40, 40, 40, 40);
-        setContentView(tv);
-
-        tv.postDelayed(() -> {
-            ensureVpnPermission();
-            startActivity(new Intent(this, PaymentActivity.class));
-            finish();
-        }, 1200);
-    }
-
-    private void ensureVpnPermission() {
-        Intent vpnIntent = VpnService.prepare(this);
-        if (vpnIntent != null) {
-            startActivityForResult(vpnIntent, VPN_REQUEST_CODE);
-        } else if (EngineState.isBlocked(this)) {
-            startService(new Intent(this, BetBlockVpnService.class));
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == VPN_REQUEST_CODE && resultCode == RESULT_OK) {
-            if (EngineState.isBlocked(this)) {
-                startService(new Intent(this, BetBlockVpnService.class));
-            }
-        }
+        finish();
     }
 }
