c021db954f8f022888609ff3897158912d525fdd
Mig-Lene31 <Mislene.souss789@gmail.com>
2026-01-19 21:46:22 -0300

    DEBUG: heartbeat log para provar serviço ativo

--- android/app/src/main/java/com/stopbet/app/StopHeartService.java
+++ android/app/src/main/java/com/stopbet/app/StopHeartService.java
@@ -4,14 +4,18 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
+import android.util.Log;
 
 public class StopHeartService extends Service {
+
+    private static final String TAG = "KAIROS_HEART";
 
     private final Handler handler = new Handler();
 
     private final Runnable loop = new Runnable() {
         @Override
         public void run() {
+            Log.d(TAG, "HEARTBEAT ATIVO");
             handler.postDelayed(this, 3000);
         }
     };
@@ -20,11 +24,13 @@ public class StopHeartService extends Service {
     public void onCreate() {
         super.onCreate();
         MotorStateStore.setRunning(this, true);
+        Log.d(TAG, "SERVICE STARTED");
         handler.post(loop);
     }
 
     @Override
     public void onDestroy() {
+        Log.d(TAG, "SERVICE STOPPED");
         MotorStateStore.setRunning(this, false);
         handler.removeCallbacks(loop);
         super.onDestroy();
