d6c97c6970af2673e5b1fd1f082aaba2712f72df
Mig-Lene31 <Mislene.souss789@gmail.com>
2026-01-19 21:35:51 -0300

    DEBUG: remove foreground service para diagnosticar crash

--- android/app/src/main/java/com/stopbet/app/StopHeartService.java
+++ android/app/src/main/java/com/stopbet/app/StopHeartService.java
@@ -4,7 +4,6 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Handler;
 import android.os.IBinder;
-import com.stopbet.app.runtime.RuntimeHeartbeat;
 
 public class StopHeartService extends Service {
 
@@ -13,15 +12,6 @@ public class StopHeartService extends Service {
     private final Runnable loop = new Runnable() {
         @Override
         public void run() {
-
-            if (!MotorStateStore.isRunning(StopHeartService.this)) {
-                stopSelf();
-                return;
-            }
-
-            double balance = AppState.getBalance(StopHeartService.this);
-            RuntimeHeartbeat.tick(StopHeartService.this, balance);
-
             handler.postDelayed(this, 3000);
         }
     };
@@ -29,16 +19,13 @@ public class StopHeartService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        startForeground(1, ForegroundNotify.create(this, "Motor ATIVO"));
         MotorStateStore.setRunning(this, true);
-        SessionStore.start(this);
         handler.post(loop);
     }
 
     @Override
     public void onDestroy() {
         MotorStateStore.setRunning(this, false);
-        SessionStore.clear(this);
         handler.removeCallbacks(loop);
         super.onDestroy();
     }
