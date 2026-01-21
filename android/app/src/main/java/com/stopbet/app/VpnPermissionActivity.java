3a04908d084e82059a09c7e812415a0351d20472
Mig-Lene31 <Mislene.souss789@gmail.com>
2026-01-21 08:59:52 -0300

    CORE: pedido obrigatório de permissão VPN no fluxo do motor

---
+++ android/app/src/main/java/com/stopbet/app/VpnPermissionActivity.java
@@ -0,0 +1,33 @@
+package com.stopbet.app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.VpnService;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class VpnPermissionActivity extends Activity {
+
+    private static final int REQ_VPN = 1001;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = VpnService.prepare(this);
+        if (intent != null) {
+            startActivityForResult(intent, REQ_VPN);
+        } else {
+            onActivityResult(REQ_VPN, RESULT_OK, null);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQ_VPN && resultCode == RESULT_OK) {
+            startService(new Intent(this, BetBlockVpnService.class));
+            Toast.makeText(this, "VPN ATIVADA", Toast.LENGTH_SHORT).show();
+        }
+        finish();
+    }
+}
